6fbcb4f872ac7b8deeafcbc071044a058e7b243c
CyanFroste <cyan14froste@gmail.com>
2024-09-24 14:09:53 +0530

    add menu

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,6 @@
+import { clsx, type ClassValue } from 'clsx'
+import { twMerge } from 'tailwind-merge'
+
 type Rgb = { r: number; g: number; b: number }
 
 function hexToRgb(hex: string): Rgb {
@@ -13,4 +16,8 @@ export function getTextColorFromBgColor(color: string | Rgb) {
   const { r, g, b } = typeof color === 'string' ? hexToRgb(color) : color
   const luminance = 0.2126 * (r / 255) + 0.7152 * (g / 255) + 0.0722 * (b / 255)
   return luminance > 0.5 ? 'black' : 'white'
+}
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
 }
